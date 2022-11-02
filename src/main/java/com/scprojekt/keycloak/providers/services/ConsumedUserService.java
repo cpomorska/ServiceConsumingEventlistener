918cd1a3ae6c6692154289403963077767d3b2fa
Claudius Pomorska <claudius.pomorska@scprojekt.com>
2022-11-03 00:17:30 +0100

    Changed ConsumedUserService.java, optimized structure

--- src/main/java/com/scprojekt/keycloak/providers/services/ConsumedUserService.java
+++ src/main/java/com/scprojekt/keycloak/providers/services/ConsumedUserService.java
@@ -9,6 +9,7 @@ import jakarta.inject.Inject;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import org.apache.http.HttpStatus;
 
 import javax.ws.rs.core.MediaType;
 import java.net.URI;
@@ -28,6 +29,8 @@ import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 @AllArgsConstructor(onConstructor_ = @Inject)
 public class ConsumedUserService {
 
+    public static final String AMPERSAND_STRING = "&";
+    public static final String EQUALS_STRING = "=";
     private final ConsumedUserServiceClient consumedUserServiceClient;
 
     public UserServiceToken getUserServiceToken(ServiceUser serviceUser){
@@ -49,25 +52,32 @@ public class ConsumedUserService {
     @SneakyThrows
     private UserServiceToken makeRequestToUserService(String serviceUri, Map<String, String> postParameters){
 
-        String form = postParameters.entrySet()
-                .stream()
-                .filter(e -> e.getValue() != null)
-                .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
-                .collect(Collectors.joining("&"));
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(form))
-                .uri(URI.create(serviceUri))
-                .header(AUTHORIZATION, consumedUserServiceClient.getAuthorizationHeader())
-                .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                .build();
-
+        HttpRequest request = createHttpRequest(serviceUri, postParameters);
         HttpResponse<String> httpResponse = consumedUserServiceClient.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
-        if (httpResponse.statusCode() == 200) {
+        if (httpResponse.statusCode() == HttpStatus.SC_OK) {
             return consumedUserServiceClient.getObjectMapper().readValue(httpResponse.body(), UserServiceToken.class);
         }
 
         return UserServiceToken.builder().requeststatus(Requeststatus.TOKEN_ERROR).build();
+    }
+
+    private HttpRequest createHttpRequest(String serviceUri, Map<String, String> postParameters) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(getUrlParameterFormEncodedFromPostParameterMap(postParameters)))
+                .uri(URI.create(serviceUri))
+                .header(AUTHORIZATION, consumedUserServiceClient.getAuthorizationHeader())
+                .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .build();
+        return request;
+    }
+
+    private static String getUrlParameterFormEncodedFromPostParameterMap(Map<String, String> postParameters) {
+        String urlFormToEncode = postParameters.entrySet()
+                .stream()
+                .filter(e -> e.getValue() != null)
+                .map(e -> e.getKey() + EQUALS_STRING + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                .collect(Collectors.joining(AMPERSAND_STRING));
+        return urlFormToEncode;
     }
 }
